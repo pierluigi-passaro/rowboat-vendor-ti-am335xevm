7008af243457450f2138e4a9f584c52cef38d06a
Pankaj Bharadiya <pankaj.bharadiya@ti.com>
2012-01-19 12:34:34 +0530

    Initial configuration for am335xevm
    Signed-off-by: Pankaj Bharadiya <pankaj.bharadiya@ti.com>

---
+++ device.mk
@@ -0,0 +1,49 @@
+#
+# Copyright (C) 2011 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+PRODUCT_COPY_FILES := \
+	device/ti/am335xevm/init.rc:root/init.rc \
+	device/ti/am335xevm/vold.fstab:system/etc/vold.fstab
+
+PRODUCT_PROPERTY_OVERRIDES := \
+	wifi.interface=wlan0 \
+	hwui.render_dirty_regions=false
+
+PRODUCT_CHARACTERISTICS := tablet,nosdcard
+
+DEVICE_PACKAGE_OVERLAYS := \
+    device/ti/am335xevm/overlay
+
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+PRODUCT_PACKAGES += \
+	librs_jni \
+	com.android.future.usb.accessory
+
+PRODUCT_PACKAGES += \
+	libaudioutils
+
+PRODUCT_PACKAGES += \
+	dhcpcd.conf \
+	TQS_D_1.7.ini \
+	calibrator
+
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+	make_ext4fs
+
+$(call inherit-product, frameworks/base/build/tablet-dalvik-heap.mk)
