af893a7e030a780b21f4a3d628ba75062b4c6c39
Venkateswarlu Vinjamuri <x0152483@ti.com>
2012-09-26 16:31:40 +0530

    Enable WiLink8 driver support
    WiLink8 driver uses the wpa from wpa_supplicant_8
    
    Signed-off-by: Venkateswarlu Vinjamuri <x0152483@ti.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,12 +53,22 @@ BOARD_FLASH_BLOCK_SIZE := 4096
 USES_TI_MAC80211 := true
 ifdef USES_TI_MAC80211
 BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
-WPA_SUPPLICANT_VERSION           := VER_0_8_X_TI
 BOARD_HOSTAPD_DRIVER             := NL80211
 BOARD_WLAN_DEVICE                := wl12xx_mac80211
 BOARD_SOFTAP_DEVICE              := wl12xx_mac80211
+
+ifeq ($(WILINK), wl18xx)
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wl12xx
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_wl12xx
+WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wlcore_sdio.ko"
+WIFI_DRIVER_MODULE_NAME          := "wlcore_sdio"
+else
+WPA_SUPPLICANT_VERSION           := VER_0_8_X_TI
 WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wl12xx_sdio.ko"
 WIFI_DRIVER_MODULE_NAME          := "wl12xx_sdio"
+endif
+
 WIFI_FIRMWARE_LOADER             := ""
 COMMON_GLOBAL_CFLAGS += -DUSES_TI_MAC80211
 endif
