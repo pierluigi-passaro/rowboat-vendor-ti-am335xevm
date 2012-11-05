e6ff65e428fcd971af2d102341be6613039a8bcd
Abhijit Kumbhar <x0112741@ti.com>
2012-11-04 20:59:55 -0600

    Use external/wpa_supplicant_8_ti for WL18xx
    Change-Id: I4dc4431696ba68c23c2c789b85b6fef00d715efd
    Signed-off-by: Abhijit Kumbhar <x0112741@ti.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -56,15 +56,12 @@ BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
 BOARD_HOSTAPD_DRIVER             := NL80211
 BOARD_WLAN_DEVICE                := wl12xx_mac80211
 BOARD_SOFTAP_DEVICE              := wl12xx_mac80211
+WPA_SUPPLICANT_VERSION           := VER_0_8_X_TI
 
 ifeq ($(WILINK), wl18xx)
-WPA_SUPPLICANT_VERSION           := VER_0_8_X
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wl12xx
-BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_wl12xx
 WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wlcore_sdio.ko"
 WIFI_DRIVER_MODULE_NAME          := "wlcore_sdio"
 else
-WPA_SUPPLICANT_VERSION           := VER_0_8_X_TI
 WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wl12xx_sdio.ko"
 WIFI_DRIVER_MODULE_NAME          := "wl12xx_sdio"
 endif
