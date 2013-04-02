a6166220573aeaec91a2a702efe4863cfe1e77c5
Pankaj Bharadiya <pankaj.bharadiya@ti.com>
2013-04-02 13:27:55 +0530

    device.mk: add telephony gsm permission file
    Signed-off-by: Pankaj Bharadiya <pankaj.bharadiya@ti.com>

--- device.mk
+++ device.mk
@@ -41,7 +41,8 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
 	frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
 	frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
-	device/ti/am335xevm/android.hardware.screen.xml:system/etc/permissions/android.hardware.screen.xml
+	device/ti/am335xevm/android.hardware.screen.xml:system/etc/permissions/android.hardware.screen.xml \
+	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
 
 PRODUCT_COPY_FILES += \
 	packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
